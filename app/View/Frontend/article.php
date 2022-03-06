1e53ad1953ddb306544c759d5cb8eb1cfb446a40
Laura THAY <laura.thay@ipag.fr>
2022-03-06 22:47:28 +0100

    page article done

--- app/View/Frontend/article.php
+++ app/View/Frontend/article.php
@@ -1,6 +1,20 @@
-<?php
-echo $article->getTitle(); ?><br><?php
-echo $article->getContent();
-?>
-
 <a href="/">Home</a>
+
+<div class="blog-header-container">
+    <div class="blog-header">
+                <div class="blog-article header-article">
+                    <div class="blog-big__title">
+                        <?php
+                        echo $article->getTitle(); ?><br>
+                        <div class="blog-menu small-title date">
+                        <?php echo $article->getCreatedAt()->format('D, d M Y H:i') ?></div>
+                        </div>
+                            <div class="blog-article">
+                            <div class="blog-detail">
+                        <?php
+                        echo $article->getContent();
+                        ?>
+                    </div>
+                </div>
+    </div>
+</div>
