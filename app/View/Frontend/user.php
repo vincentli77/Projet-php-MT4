4cfc8d09c9e9915ec57e4c2c5d4d40e898602cd7
Laura THAY <laura.thay@ipag.fr>
2022-03-06 22:43:23 +0100

    page user done
    probleme avec la liste des user que je n'arrive pas a supprimer et que j'ai mis en commentaire

--- app/View/Frontend/user.php
+++ app/View/Frontend/user.php
@@ -1,17 +1,14 @@
 <a href="/">Home</a>
 
-<h1> Users list </h1>
-
-
 <form class="form" method="post">
     <h1 class="form-title">Créer un user</h1>
     <div class="grid">
         <div class="form-group">
-            <input type="text" name="userId" id="name" placeholder="User Id ">
+            <input type="text" name="userId" id="userId" placeholder="User Id ">
             <!-- <label for="authorId"> Author Id : <input name="authorId" type="text"></label>  -->
         </div>
         <div class="form-group">
-            <input type="text" name="name" id="name" placeholder="User Name">
+            <input type="text" name="name" id="user name" placeholder="User Name">
             <!-- <label for="title"> Titre: <input name="title" type="text"></label>  -->
         </div>
         <div class="form-group">
@@ -28,15 +25,57 @@
     </div>
 </form>
 
-<?php
+ <?php
 // /** @var $article \App\Entity\Post */
 
+
+// probleme avec ce bloc, si je le supprime ca fait tout peter x) 
 foreach ($users as $key => $user) {
     echo $user->getId();
     echo $user->getNickname();
     echo $user->getRank();
-    ?>
-<a href="/modifyUser/<?php echo $user->getId()?>">Modifier cet utilisateur</a>
-<a href="/deleteUser/<?php echo $user->getId()?>">Supprimer cet utilisateur</a>
+     ?>
+ <a href="/modifyUser/<?php echo $user->getId()?>">Modifier cet utilisateur</a> 
+ <a href="/deleteUser/<?php echo $user->getId()?>">Supprimer cet utilisateur</a>
 
 <?php } ?>
+
+<!-- USERS -->
+<h1> Users list </h1>
+
+<div class="blog-header-container">
+        <!----------------- l'article 1 ------------------>
+
+        <?php
+
+        
+        foreach ($users as $key => $user) {
+           if(count($users) == 0) { //si y a 0  resultat
+                echo "Aucun articles"; // on affiche aucun article
+            } else if($key<3){   
+            // sinon
+                ?>
+        <div class="blog-header">
+            <div class="blog-article header-article">
+                <div class="blog-big__title"><?php echo $user->getNickname();?></div>
+                <div class="blog-menu small-title date">
+                    <?php echo $user->getCreatedAt()->format('D, d M Y H:i') ?></div>
+            </div>
+
+            <div class="blog-article">
+                <div class="blog-detail">
+                    <span><a style="font-size:initial;font-weight:bold;border-top:unset;padding-top:unset"
+                            href="/modifyUser/<?php echo $user->getId()?>">Modifier cet user</a>
+                    </span>
+
+                    <span> <a style="font-size:initial;font-weight:bold;border-top:unset;padding-top:unset"
+                            href="/deleteUser/<?php echo $user->getId()?>">Supprimer cet user</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <?php
+            }
+        }
+        ?>
+    </div>
