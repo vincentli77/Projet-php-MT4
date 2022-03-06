c18e81bf40e56f20656df55ff1dc66f113d356e3
VLI77 <vincent.li@hetic.net>
2022-03-06 20:44:55 +0100

    recover

--- app/View/Frontend/createArticle.php
+++ app/View/Frontend/createArticle.php
@@ -1,8 +1,27 @@
-<h1>Créer des articles</h1>
-<form method="post">
-    <label for="title"> Titre: <input name="title" type="text"></label>
-    <label for="content"> Content : <input name="content" type="text"></label>
-    <label for="authorId"> Author Id : <input name="authorId" type="text"></label>
-    <input type="submit" value="Envoyer" />
-</form>
 <a href="/">Home</a>
+
+
+<form class="form" method="post">
+    <h1 class="form-title">Créer un article</h1>
+    <div class="grid">
+        <div class="form-group">
+            <input type="text" name="authorId" id="name" placeholder="Author Id ">
+            <!-- <label for="authorId"> Author Id : <input name="authorId" type="text"></label>  -->
+        </div>
+        <div class="form-group">
+            <input type="text" name="title" id="title" placeholder="Titre:">
+            <!-- <label for="title"> Titre: <input name="title" type="text"></label>  -->
+        </div>
+        <div class="form-group">
+            <input type="text" name="content" id="content" placeholder="Content">
+            <!-- <label for="content"> Content : <input name="content" type="text"></label>  -->
+        </div>
+        <div class="form-group">
+            <!-- <textarea name="message" id="message" placeholder="Your message goes here"></textarea> -->
+            <input type="submit" value="Envoyer" />
+        </div>
+        <button type="submit" class="btn" value="Envoyer"><a class="submit" href="http://localhost:5555/">Votre mon
+                article prendre son envol</a></button>
+        <!-- <input type="submit" value="Envoyer" /> -->
+    </div>
+</form>
